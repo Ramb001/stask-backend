a5dcbab110ff90f24c2ca2bd73731df0089f920a
Artem <artem.rmb@bk.ru>
2024-03-30 18:39:47 +0300

    fix: chaged port

--- main.py
+++ main.py
@@ -240,4 +240,4 @@ async def delete_worker(request: DeleteWorker):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=9090, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
